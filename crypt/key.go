a220eee60cc8f919dda8d0a134ee335e341a2b99
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-02-17 18:24:11 +0900

    Add key.LoadKey(name string, pemBytes []byte)

--- crypt/key.go
+++ crypt/key.go
@@ -5,7 +5,12 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 )
+
+var ErrMissingPem = errors.New("invalid pem (couldn't decode)")
+var ErrInvalidPem = errors.New("invalid pem (type should be RSA PUBLIC KEY, PUBLIC KEY, RSA PRIVATE KEY, or PRIVATE KEY)")
+var ErrNotRsaKey = errors.New("invalid pem (key is not RSA public key or private key)")
 
 type Key struct {
 	Name    string
@@ -26,6 +31,40 @@ func NewPublicKey(name string, rsaPublicKey *rsa.PublicKey) *Key {
 	return &Key{
 		Name:   name,
 		Public: rsaPublicKey,
+	}
+}
+
+func LoadKey(name string, pemBytes []byte) (*Key, error) {
+	pem, _ := pem.Decode(pemBytes)
+	if pem == nil {
+		return nil, ErrMissingPem
+	}
+
+	switch pem.Type {
+	case "PUBLIC KEY", "RSA PUBLIC KEY":
+		parsedKey, err := x509.ParsePKIXPublicKey(pem.Bytes)
+		if err != nil {
+			return nil, err
+		}
+
+		var pubKey *rsa.PublicKey
+		var ok bool
+		if pubKey, ok = parsedKey.(*rsa.PublicKey); !ok {
+			return nil, ErrNotRsaKey
+		}
+
+		return NewPublicKey(name, pubKey), nil
+
+	case "PRIVATE KEY", "RSA PRIVATE KEY":
+		privateKey, err := x509.ParsePKCS1PrivateKey(pem.Bytes)
+		if err != nil {
+			return nil, err
+		}
+
+		return NewPrivateKey(name, privateKey), nil
+
+	default:
+		return nil, ErrInvalidPem
 	}
 }
 
--- crypt/key_test.go
+++ crypt/key_test.go
@@ -1,7 +1,6 @@
 package crypt
 
 import (
-	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
@@ -10,10 +9,49 @@ import (
 	"testing"
 )
 
+var testRsaPrivateKey = []byte(`-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQDE0H3AjeUvlOA5ueZ1q6hukF+aRFbW2h8qW2OIw88+EN4qLani
+lTvTUO3V91hGhHe2CnnUOey1iAHnSPGx66XW3oN/Wuk+wK1tg1ivcCLHIOlRu22g
+8DuS8TC92jhjkFVCgGasXNFGECiyF6J9WsYrF6F/OKvUVpEjWgyRMPMMuQIDAQAB
+AoGAMOlbhyH8ZhHKk64GfxHU/v00NSNsrWJxwlYJ63A2LceFXtgQUzYhMwf2w2j/
+8C51jbEWy85FbGvLhU4UetIEWW0OK5Y+J2juGD0ez1FX+EzmiO+khpGtYQ6OY56a
+3g4FPsUuCj1gw2oBDDQ2e38RyqY9Nj3PWo4H5Y7ZbSWwSQ0CQQDSNABnC7AiM2K3
+5uXqZiXx68RoLrYtGkXhgyZBIUZ+g6nbhBqpPEI9pql55yCjmx/zeY6VVipOffO2
+EEUpdnG/AkEA77G9SK8lqxMeH+GRL70jYNXBqdxYhKrWlFzom+VrHIyo//limocH
+dPJiEEIyPJQXeru2r2mWxVg98q+j3CUvhwJAIzebKaiHpfM+Atmog5EBonqBuYK5
++ux/8LxsWFUe3mtoteJ4JQp3fqTBmC7lBQQkYkJnZRW+mM/5WPN44u15OQJBAJPO
+Wbehcav9vPzR3vK+QjurdKHnI5qjsnCInlPL8/IF9wzp3tkFXR7LfJckCtB6TcQ8
+Ttn6VaPZ11F456WQNK8CQETVQARcp/v4bWtVHfJKyBcx92FkclVNXae5aHpmvIjI
+LUu9LpYOrkcaL1d7SFPhWZUsI+crYKuLAb9tXG/AnJY=
+-----END RSA PRIVATE KEY-----`)
+
+var testRsaPublicKey = []byte(`-----BEGIN RSA PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDE0H3AjeUvlOA5ueZ1q6hukF+a
+RFbW2h8qW2OIw88+EN4qLanilTvTUO3V91hGhHe2CnnUOey1iAHnSPGx66XW3oN/
+Wuk+wK1tg1ivcCLHIOlRu22g8DuS8TC92jhjkFVCgGasXNFGECiyF6J9WsYrF6F/
+OKvUVpEjWgyRMPMMuQIDAQAB
+-----END RSA PUBLIC KEY-----`)
+
+var testEcdsaPrivateKey = []byte(`-----BEGIN PRIVATE KEY-----
+MGgCAQEEHF+pP6QjO+LH97mzJlaiqZ1y5DynKEjUSXy7hVSgBwYFK4EEACGhPAM6
+AASwJR+5yutBOBaKlxjheM+VPm4kfeXoxnjN85OHAfYeyEPS95kZZKqbpvX8d8NF
+Z4+YLPZEMaBs7g==
+-----END PRIVATE KEY-----`)
+
+var testEcdsaPublicKey = []byte(`-----BEGIN PUBLIC KEY-----
+ME4wEAYHKoZIzj0CAQYFK4EEACEDOgAEsCUfucrrQTgWipcY4XjPlT5uJH3l6MZ4
+zfOThwH2HshD0veZGWSqm6b1/HfDRWePmCz2RDGgbO4=
+-----END PUBLIC KEY-----`)
+
 var rsaKey rsa.PrivateKey
 
 func TestMain(m *testing.M) {
-	priv, err := rsa.GenerateKey(rand.Reader, 1024)
+	pem, _ := pem.Decode(testRsaPrivateKey)
+	if pem == nil {
+		panic("invalid pem")
+	}
+
+	priv, err := x509.ParsePKCS1PrivateKey(pem.Bytes)
 	if err != nil {
 		panic(err)
 	}
@@ -21,7 +59,6 @@ func TestMain(m *testing.M) {
 
 	os.Exit(m.Run())
 }
-
 func TestNewPrivateKey(t *testing.T) {
 	key := NewPrivateKey("foo", &rsaKey)
 
@@ -55,6 +92,118 @@ func TestNewPublicKey(t *testing.T) {
 	}
 }
 
+func TestLoadKeyPublic(t *testing.T) {
+	key, err := LoadKey("foo", testRsaPublicKey)
+
+	if err != nil {
+		t.Errorf("error %#v", err)
+	}
+
+	if key.Name != "foo" {
+		t.Errorf("unexpected key.Name %#v", key.Name)
+	}
+
+	if key.Private != nil {
+		t.Errorf("unexpected key.Private %#v", key.Private)
+	}
+
+	if key.Public.E != rsaKey.Public().(*rsa.PublicKey).E {
+		t.Errorf("unexpected key.Public", key.Public)
+	}
+}
+
+func TestLoadKeyPublicButNotRsa(t *testing.T) {
+	key, err := LoadKey("foo", testEcdsaPublicKey)
+
+	if err != ErrNotRsaKey {
+		t.Errorf("unexpected error %#v", err)
+	}
+
+	if key != nil {
+		t.Errorf("unexpected key %#v", err)
+	}
+}
+
+func TestLoadKeyPublicButInvalid(t *testing.T) {
+	// broken
+	key, err := LoadKey("foo", []byte(`-----BEGIN RSA PUBLIC KEY-----
+Wbehcav9vPzR3vK+QjurdKHnI5qjsnCInlPL8/IF9wzp3tkFXR7LfJckCtB6TcQ8
+Ttn6VaPZ11F456WQNK8CQETVQARcp/v4bWtVHfJKyBcx92FkclVNXae5aHpmvIjI
+LUu9LpYOrkcaL1d7SFPhWZUsI+crYKuLAb9tXG/AnJY=
+-----END RSA PRIVATE KEY-----`))
+
+	if err == nil {
+		t.Errorf("unexpected error %#v", err)
+	}
+
+	if key != nil {
+		t.Errorf("unexpected key %#v", err)
+	}
+}
+
+func TestLoadKeyPrivate(t *testing.T) {
+	key, err := LoadKey("foo", testRsaPrivateKey)
+
+	if err != nil {
+		t.Errorf("error %#v", err)
+	}
+
+	if key.Name != "foo" {
+		t.Errorf("unexpected key.Name %#v", key.Name)
+	}
+
+	if key.Private == nil {
+		t.Errorf("unexpected key.Private %#v", key.Private)
+	}
+
+	if key.Private.E != rsaKey.E {
+		t.Errorf("unexpected key.Private %#v", key.Private)
+	}
+}
+
+func TestLoadKeyPrivateButInvalid(t *testing.T) {
+	// broken
+	key, err := LoadKey("foo", []byte(`-----BEGIN RSA PRIVATE KEY-----
+GSIb3DQEBAQUAA4GNADCBiQKBgQDE0H3AjeUvlOA5ueZ1q6hukF+aRFbW2h8qW2O
+Iw88+EN4qLanilTvTUO3V91hGhHe2CnnUOey1iAHnSPGx66XW3oNWuk+wK1tg1iv
+cCLHIOlRu22g8DuS8TC92jhjkFVCgGasXNFGECiyF6J9WsYrF6FOKvUVpEjWgyRM
+PMMuQIDAQAB
+-----END RSA PUBLIC KEY-----`))
+
+	if err == nil {
+		t.Errorf("unexpected error %#v", err)
+	}
+
+	if key != nil {
+		t.Errorf("unexpected key %#v", err)
+	}
+}
+
+func TestLoadKeyMissing(t *testing.T) {
+	key, err := LoadKey("foo", []byte{})
+
+	if err != ErrMissingPem {
+		t.Errorf("unexpected error %#v", err)
+	}
+
+	if key != nil {
+		t.Errorf("unexpected key %#v", err)
+	}
+}
+
+func TestLoadKeyInvalid(t *testing.T) {
+	key, err := LoadKey("foo", []byte(`-----BEGIN SOMETHING KEY-----
+PMMuQIDAQAB
+-----END SOMETHING KEY-----`))
+	if err != ErrMissingPem {
+		t.Errorf("unexpected error %#v", err)
+	}
+
+	if key != nil {
+		t.Errorf("unexpected key %#v", err)
+	}
+}
+
 func TestGenerateKey(t *testing.T) {
 	key, err := GenerateKey("foo", 1024)
 
@@ -79,6 +228,10 @@ func TestPublicPem(t *testing.T) {
 
 	pem, _ := pem.Decode(pemBytes)
 
+	if pem == nil {
+		t.Errorf("couldn't decode pem: %#v", pemBytes)
+	}
+
 	if pem.Type != "PUBLIC KEY" {
 		t.Errorf("pem unexpected type %#v", pem.Type)
 	}
@@ -97,6 +250,10 @@ func TestPrivatePem(t *testing.T) {
 	pemBytes := key.PrivatePem()
 
 	pem, _ := pem.Decode(pemBytes)
+
+	if pem == nil {
+		t.Errorf("couldn't decode pem: %#v", pemBytes)
+	}
 
 	if pem.Type != "PRIVATE KEY" {
 		t.Errorf("pem unexpected type %#v", pem.Type)
