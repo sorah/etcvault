4277137d0962cbad576b9749b7c9aa8da7d4f9b4
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-03-02 21:46:35 +0900

    keychain: Cache loaded keys

--- keys/keychain.go
+++ keys/keychain.go
@@ -9,23 +9,38 @@ import (
 var ErrKeyNotFound = errors.New("couldn't find specified key")
 
 type Keychain struct {
-	Path string
+	Path  string
+	Cache map[string]*Key
 }
 
 func NewKeychain(path string) *Keychain {
 	return &Keychain{
-		Path: path,
+		Path:  path,
+		Cache: make(map[string]*Key),
 	}
 }
 
 func (keychain *Keychain) Find(name string) (*Key, error) {
+	if key, ok := keychain.Cache[name]; ok {
+		return key, nil
+	}
+
 	privateKeyPath := path.Join(keychain.Path, name+".pem")
 	publicKeyPath := path.Join(keychain.Path, name+".pub")
 
 	if _, err := os.Stat(privateKeyPath); err == nil {
-		return LoadKeyFromFile(privateKeyPath)
+		key, err := LoadKeyFromFile(privateKeyPath)
+		if err != nil {
+			return nil, err
+		}
+		keychain.Cache[name] = key
+		return key, nil
 	} else if _, err := os.Stat(publicKeyPath); err == nil {
-		return LoadKeyFromFile(publicKeyPath)
+		key, err := LoadKeyFromFile(publicKeyPath)
+		if err != nil {
+			return nil, err
+		}
+		return key, nil
 	} else {
 		return nil, ErrKeyNotFound
 	}
