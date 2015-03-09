59b1853e0ef1dc4b0da8c003251732d317f45978
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-03-09 16:23:31 +0900

    add `etcvault transform` command

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/codegangsta/cli"
+	"github.com/sorah/etcvault/engine"
 	"github.com/sorah/etcvault/keys"
+	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -26,6 +30,21 @@ func main() {
 					Name:  "bits",
 					Value: 2048,
 					Usage: "RSA key bit length to generate",
+				},
+			},
+		},
+		{
+			Name:   "transform",
+			Usage:  "transform ETCVAULT* strings (from argument or stdin) to appropriate strings",
+			Action: actionTransform,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "keychain",
+					Usage: "Path to directory for keys",
+				},
+				cli.BoolFlag{
+					Name:  "stdin",
+					Usage: "Read from stdin",
 				},
 			},
 		},
@@ -55,5 +74,50 @@ func actionKeygen(ctx *cli.Context) {
 	} else {
 		keychain := keys.NewKeychain(saveDir)
 		keychain.Save(key)
+	}
+}
+
+func actionTransform(ctx *cli.Context) {
+	keychainDir := ctx.String("keychain")
+	if keychainDir == "" {
+		fmt.Fprintln(os.Stderr, "Specify -keychain option")
+		os.Exit(1)
+	}
+
+	keychain := keys.NewKeychain(keychainDir)
+	engine := engine.NewEngine(keychain)
+
+	if ctx.Bool("stdin") {
+		reader := bufio.NewReader(os.Stderr)
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				} else {
+					panic(err)
+				}
+			}
+
+			origStr := strings.TrimRight(line, "\n")
+
+			str, err := engine.Transform(origStr)
+			if err == nil {
+				fmt.Println(str)
+			} else {
+				fmt.Println(origStr)
+				fmt.Fprintf(os.Stderr, "ERR: %s\n", err.Error())
+			}
+		}
+	} else {
+		for _, origStr := range ctx.Args() {
+			str, err := engine.Transform(origStr)
+			if err == nil {
+				fmt.Println(str)
+			} else {
+				fmt.Println(origStr)
+				fmt.Fprintf(os.Stderr, "ERR: %s", err.Error())
+			}
+		}
 	}
 }
